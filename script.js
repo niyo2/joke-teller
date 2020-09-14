30217014947cfd4193f1f552b42ddcd89d1fdf03
niyo2 <niyitanga.aime9@gmail.com>
2020-09-14 13:20:48 +0200

    adding Joke Teller  project

---
+++ script.js
@@ -0,0 +1,54 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
+// Disable/Enable Button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+//VoiceRSS Speech Function 
+function tellMe(joke) {
+    const jokeString = joke.trim().replace(/ /g, "%20");
+
+// |Voice Speech Paramaeters  
+    VoiceRSS.speech({
+        key: "2dee313468e742c1a87073a2f9aaf6f5",
+        src: jokeString,
+        hl: "en-us",
+        v: "Linda",
+        r: 0, 
+        c: "mp3",
+        f: "44khz_16bit_stereo",
+        ssml: false,
+    });
+}
+
+// Get Jokes from Joke API
+async function getJokes(){
+    let joke = "";
+    const apiUrl = "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist&type=single";
+        try{
+           const response = await fetch(apiUrl);
+           const data = await response.json();
+           // Assign One or Two Part Joke
+           if (data.setup) {
+               joke = `${data.setup} ... ${data.delivery}`;
+           }else {
+               joke = data.joke;
+           }
+
+           // Passing Joke to VoiceRSS API
+           tellMe(joke);
+
+           // Disable Button
+           toggleButton();
+        }catch(error){
+            //Catch errors Here
+            console.log("Whoops are u", error);
+    }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
+
